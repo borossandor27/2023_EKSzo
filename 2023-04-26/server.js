c603553515aed2a3d584f6b9eb93b3928bd79238
Boros Sándor <boros.sandor@dszcberegszaszi.hu>
2023-04-26 18:40:30 +0200

    menhelyes

---
+++ 2023-04-26/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+const bodyParser = require('body-parser');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'menhely'
+});
+
+connection.connect();
+
+app.get('/', function (req, res) {
+    //res.send('hello world!');
+
+});
+
+app.get('/allat/:id', function (req, res) {
+    let sql = 'SELECT * FROM `allatok` where allatid = ' + req.params.id;
+    connection.query(sql, function (err, rows) {
+        res.send(rows);
+    })
+    console.log(req.params)
+})
+app.delete('/allat/:id', function (req, res) {
+    let sql = 'DELETE FROM `allatok` where allatid = ' + req.params.id;
+    connection.query(sql, function (err, rows) {
+        res.send(rows);
+    })
+    console.log(req.params)
+})
+app.post('/allat/', function (req, res) {
+    let sql = 'INSERT INTO allat VALUES (...' + req.params.id ;
+    connection.query(sql, function (err, rows) {
+        res.send(rows);
+    })
+    console.log(req.params)
+})
+app.put('/allat/:id', function (req, res) {
+    let sql = 'UPDATE .... where allatid = ' + req.params.id;
+    connection.query(sql, function (err, rows) {
+        res.send(rows);
+    })
+    console.log(req.params)
+})
+app.get('/allatAll', function (req, res) {
+    let sql = 'SELECT * FROM `allatok`';
+    connection.query(sql, function (err, rows) {
+        res.send(rows);
+    })
+    console.log('Összes állat')
+})
+
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
